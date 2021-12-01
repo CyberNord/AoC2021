2d40a5df56992b83deb02a990a1f9780c18237bf
Rudolf Garstenauer <rgarstenauer@yahoo.com>
2021-12-01 11:39:28 +0100

    day1 v1.1 done

--- src/cybernord/aoc2021/day1/day1.kts
+++ src/cybernord/aoc2021/day1/day1.kts
@@ -1,19 +1,23 @@
 package cybernord.aoc2021.day1
 
-import cybernord.aoc2021.readContent
 import cybernord.aoc2021.readLinesAsInt
-import kotlin.coroutines.coroutineContext
+
+/*
+The Problem:
+To do this, count the number of times a depth measurement increases from the previous measurement.
+(There is no measurement before the first measurement.)
+ */
 
 var content = "./input.txt".readLinesAsInt()
 
 //println("DEBUG $content")
 
 var prev = content[0]
-val inpLen = content.size
+// val inpLen = content.size
 // println(" DEBUG  first parameter is: $prev and the overall size is $inpLen")
 
-var counter = 0;
-var debugLen = 0;
+var counter = 0
+var debugLen = 0
 
 for(i in content){
     if (i > prev){
@@ -22,7 +26,7 @@ for(i in content){
     }else{
         // println(" DEBUG  parameter is: $i and prev is $prev -->  counter stays the same at $counter")
     }
-    prev = i;
+    prev = i
     debugLen++
 }
 
