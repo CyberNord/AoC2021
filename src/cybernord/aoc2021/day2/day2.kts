949629d2bf730bfe8b3d7d5b19e2ec076f9c2dff
R_Gar <62029482+CyberNord@users.noreply.github.com>
2021-12-02 16:11:44 +0100

    Update day2.kts

--- src/cybernord/aoc2021/day2/day2.kts
+++ src/cybernord/aoc2021/day2/day2.kts
@@ -12,14 +12,9 @@ It seems like the submarine can take a series of commands like forward 1, down 2
 forward X increases the horizontal position by X units.
 down X increases the depth by X units.
 up X decreases the depth by X units.
-Note that since you're on a submarine, down and up affect your depth,
-and so they have the opposite result of what you might expect.
-
-The submarine seems to already have a planned course (your puzzle input).
-You should probably figure out where it's going.
+Note: down and up affect your depth inverse 
 
 Your horizontal position and depth both start at 0.
-Calculate the horizontal position and depth you would have after following the planned course.
 What do you get if you multiply your final horizontal position by your final depth?
  */
 
